8a9b0fa6ef9076181c5652127b56f25addd15d5d
Pavel <p.petrunkin@dunice.net>
2019-06-27 15:18:51 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function AddListItem() {
+    if ($(".button-add"))
+    {
+        if ($(" .text-input" ).replace(/\s+/g, '') >=1) {
+            
+        }
+    }
+
+}
+
+// 1 array Completed
+// 2 array Active
+// 3 array General
