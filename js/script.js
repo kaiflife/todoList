8923c07361f289196b8a5973f68a74085f7882a8
Pavel <p.petrunkin@dunice.net>
2019-06-27 17:01:29 +0300

    create light-design, add/delete items

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
-function AddListItem() {
-    if ($(".button-add"))
-    {
-        if ($(" .text-input" ).replace(/\s+/g, '') >=1) {
-            
+
+    $(document).ready(function () {
+        $(document).on('click','.button-add',function () {
+            $('#list').append("<li>" + $("#text-input[name=task]").val() + " <a href='#' " +
+                "class='close' aria-hidden='true'>&times;</a></li>");
+        });
+        $("body").on('click', '#list a', function () {
+            $(this).closest("li").remove();
+        });
+
+        if($(document).on('click','li :not(.checked)')){
+            alert('click on checked li');
         }
-    }
 
-}
+        //This work code!!
+        // $(document).on('click','li :not(.checked)',function () {
+        //     $('li').addClass("checked");
+        // });
 
-// 1 array Completed
-// 2 array Active
-// 3 array General
+
+    });
+
+
+
+    // Add a "checked" symbol when clicking on a list item
+
+    // var list = document.querySelector('ul');
+    // list.addEventListener('click', function(ev) {
+    //     if (ev.target.tagName === 'LI') {
+    //         ev.target.classList.toggle('checked');
+    //     }
+    // }, false);
+
+
+// 1 array Completed :checked (no : checked display:none, :checked display:initial)
+// 2 array Active no :checked (np :checked display: initial, :checked display:none)
+// 3 array General li: display: initial;
