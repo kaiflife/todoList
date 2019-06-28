73a63f39058b06c561f25fbe2a20fbf5b855713f
Pavel <p.petrunkin@dunice.net>
2019-06-28 11:57:59 +0300

    add item to list on enterButton

--- js/helper.js
+++ js/helper.js
@@ -1,53 +1,37 @@
-// Create a "close" button and append it to each list item
-let myNodelist = document.getElementsByTagName("LI");
-let i;
-for (i = 0; i < myNodelist.length; i++) {
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
 
-// Click on a close button to hide the current list item
-let close = document.getElementsByClassName("close");
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        let div = this.parentElement;
-        div.style.display = "none";
+$(document).ready(function () {
+    $(document).on('click','.button-add',function () {
+        $('#list').append("<li>" + $("#text-input[name=task]").val() + " <a href='#' " +
+          "class='close' aria-hidden='true'>&times;</a></li>");
+    });
+    $("body").on('click', '#list a', function () {
+        $(this).closest("li").remove();
+    });
+
+    if($(document).on('click','li :not(.checked)')){
+        alert('click on checked li');
     }
-}
+
+    //This work code!!
+    // $(document).on('click','li :not(.checked)',function () {
+    //     $('li').addClass("checked");
+    // });
+
+
+});
+
+
 
 // Add a "checked" symbol when clicking on a list item
-let list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("text-input").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-        alert("You must write something!");
-    } else {
-        document.getElementById("list").appendChild(li);
-    }
-    document.getElementById("text-input").value = "";
 
-    let span = document.createElement("SPAN");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+// var list = document.querySelector('ul');
+// list.addEventListener('click', function(ev) {
+//     if (ev.target.tagName === 'LI') {
+//         ev.target.classList.toggle('checked');
+//     }
+// }, false);
 
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            let div = this.parentElement;
-            div.style.display = "none";
-        }
-    }
-}
+
+// 1 array Completed :checked (no : checked display:none, :checked display:initial)
+// 2 array Active no :checked (np :checked display: initial, :checked display:none)
+// 3 array General li: display: initial;
--- js/script.js
+++ js/script.js
@@ -1,37 +1,65 @@
 
-    $(document).ready(function () {
-        $(document).on('click','.button-add',function () {
-            $('#list').append("<li>" + $("#text-input[name=task]").val() + " <a href='#' " +
-                "class='close' aria-hidden='true'>&times;</a></li>");
-        });
-        $("body").on('click', '#list a', function () {
-            $(this).closest("li").remove();
-        });
+$(document).ready(function () {
+  $(document).on('click','.button-add',function () {
+    $('#list').append("<li>" + $('#text-input[name=task]').val() + ' <a href=\'#\' '
+      + 'class=\'close\' aria-hidden=\'true\'>&times;</a></li>');
+  });
+  $('body').on('click', '#list a', function() {
+    $(this).closest('li')
+      .remove();
+  });
 
-        if($(document).on('click','li :not(.checked)')){
-            alert('click on checked li');
-        }
+  if($(document).on('click', 'li :not(.checked)')){
+    alert('click on checked li');
+  }
 
-        //This work code!!
-        // $(document).on('click','li :not(.checked)',function () {
-        //     $('li').addClass("checked");
-        // });
-
-
-    });
-
-
-
-    // Add a "checked" symbol when clicking on a list item
-
-    // var list = document.querySelector('ul');
-    // list.addEventListener('click', function(ev) {
-    //     if (ev.target.tagName === 'LI') {
-    //         ev.target.classList.toggle('checked');
-    //     }
-    // }, false);
+  /*
+   *This work code!!
+   * $(document).on('click','li :not(.checked)',function () {
+   *     $('li').addClass("checked");
+   * });
+   */
 
 
-// 1 array Completed :checked (no : checked display:none, :checked display:initial)
-// 2 array Active no :checked (np :checked display: initial, :checked display:none)
-// 3 array General li: display: initial;
+});
+
+
+//  Append li to ul
+$(document).keypress(event => {
+  const keycode = event.keyCode ? event.keyCode : event.which;
+
+  // Enter pressed
+  // eslint-disable-next-line no-magic-numbers
+  if (keycode === 13) {
+    alert($('#text-input').val());
+
+    if ($('#text-input[name=task]').val().replace(/^\s*/, '')
+      .replace(/\s*$/, '') !== '') {
+
+      // Append li
+      $('#list').append("<li>" + $("#text-input[name=task]").val() + ' <a href=\'#\' ' +
+        "class='close' aria-hidden='true'>&times;</a></li>");
+    }
+  }
+
+  // Input not empty
+});
+
+
+// Add a "checked" symbol when clicking on a list item
+
+/*
+ * Var list = document.querySelector('ul');
+ * List.addEventListener('click', function(ev) {
+ *     If (ev.target.tagName === 'LI') {
+ *         Ev.target.classList.toggle('checked');
+ *     }
+ * }, false);
+ */
+
+
+/*
+ * 1 array Completed :checked (no : checked display:none, :checked display:initial)
+ * 2 array Active no :checked (np :checked display: initial, :checked display:none)
+ * 3 array General li: display: initial;
+ */
